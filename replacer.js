ac29a6608a5ff6593345a5c65b2c15b42cc17a68
bob bobson <benicloud565@icloud.com>
2015-01-15 23:49:43 -0500

    first commit

---
+++ replacer.js
@@ -0,0 +1,18 @@
+function htmlreplace(a, b, element) {    
+    if (!element) element = document.body;    
+    var nodes = element.childNodes;
+    for (var n=0; n<nodes.length; n++) {
+        if (nodes[n].nodeType == Node.TEXT_NODE) {
+            var r = new RegExp(a, 'gi');
+            nodes[n].textContent = nodes[n].textContent.replace(r, b);
+        } else {
+            htmlreplace(a, b, nodes[n]);
+        }
+    }
+}
+htmlreplace('Obama', 'Kaplan');
+htmlreplace('Barack', 'Binyamin');
+htmlreplace('google', 'googel');
+if(document.getElementById("gbqfsa") !== null){
+    document.getElementById("gbqfsa").textContent = "Kap Daddy Search";
+}
